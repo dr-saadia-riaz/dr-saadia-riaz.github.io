fb73f9eb80784a47d17f2495b8e31507cd812707
Nashmia Riaz <nashmia-riaz@hotmail.com>
2018-04-16 00:14:04 +0500

    added datepicker

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@
 $(document).ready(function(){
 	"use strict";
 
+	$('#datepicker2').datepicker();
+
 	var window_width 	 = $(window).width(),
 	window_height 		 = window.innerHeight,
 	header_height 		 = $(".default-header").height(),
